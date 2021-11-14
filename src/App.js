be10c2659768087033f3c1c3824ca4253f3a8f6c
karthik <karthikjeeva2206@gmail.com>
2021-11-14 12:21:09 +0530

    dummy student data displayed as list and alert triggered onclick

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+function clickhandle (props) {
+  alert("you clicked on" + props.name)
+}
+
+function Stud(props) {
+  return <li onClick={() => clickhandle(props)}> { props.name }</li>;
+  
+}
+
 function App() {
+  const students = ['Anil', 'Bob' , 'Clara', 'Dew', 'John', 'Ravi', 'Ram'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <ul>
+        {students.map((student) => <Stud name={student} />)}
+      </ul>
+    </>
   );
 }
 
